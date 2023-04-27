2f86f2b591549b904936aba7738996342777c015
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-04-27 08:49:16 -0300

    Londrina:27.04.23_08:49hs

---
+++ app/Models/Testamento.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Testamento extends Model
+{
+    use HasFactory;
+
+    /* Declarando qual vai ser a tabela deste modelo */
+    /* protected $table = 'testamentos'; */
+}
